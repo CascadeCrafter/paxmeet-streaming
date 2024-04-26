608490d118b498810aef19b24425a435b8fcc92c
CascadeCrafter <simomarvaila0915@gmail.com>
2024-04-26 20:29:41 +0300

    added auth check to /streaming routes

--- api/routes.go
+++ api/routes.go
@@ -22,19 +22,19 @@ func Register(config *initializers.Config, micro *fiber.App) {
 	})
 
 	micro.Route("/streaming", func(router fiber.Router) {
-		router.Post("/room/create", func(c *fiber.Ctx) error {
+		router.Post("/room/create", middleware.CheckAuth(config.Auth.Uri), func(c *fiber.Ctx) error {
 			return controllers.CreateTradingRoom(c, config)
 		})
-		router.Get("/room/get/:roomId", func(c *fiber.Ctx) error {
+		router.Get("/room/get/:roomId", middleware.CheckAuth(config.Auth.Uri), func(c *fiber.Ctx) error {
 			return controllers.GetTradingRoom(c, config)
 		})
-		router.Get("/rooms/all", func(c *fiber.Ctx) error {
+		router.Get("/rooms/all", middleware.CheckAuth(config.Auth.Uri), func(c *fiber.Ctx) error {
 			return controllers.GetAllTradingRooms(c, config)
 		})
-		router.Get("/room/join/:roomId", func(c *fiber.Ctx) error {
+		router.Get("/room/join/:roomId", middleware.CheckAuth(config.Auth.Uri), func(c *fiber.Ctx) error {
 			return controllers.JoinTradingRoom(c, config)
 		})
-		router.Delete("/room/delete/:roomId", func(c *fiber.Ctx) error {
+		router.Delete("/room/delete/:roomId", middleware.CheckAuth(config.Auth.Uri), func(c *fiber.Ctx) error {
 			return controllers.DeleteTradingRoom(c, config)
 		})
 	})
